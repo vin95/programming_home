b77661b7e65202d5f8ff6b01983a903819258bdc
vin95 <vincent.ochmann@freenet.de>
2025-06-21 13:57:57 +0200

    aufgabe 9 fertig prog 2

---
+++ pr2/OCHMANN.java.project/PR2AG/src/pr2/a09/SmileyController.java
@@ -0,0 +1,61 @@
+package pr2.a09;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class SmileyController implements ActionListener, ChangeListener{
+	protected SmileyModel model;
+	protected SmileyView view;
+	
+	public SmileyController(SmileyModel model, SmileyView view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	public void stateChanged(ChangeEvent e) {
+		Object source = e.getSource();
+		if (! (source instanceof JSlider)) {
+			System.out.println("something went wrong");
+			return;
+		}
+		JSlider slider = (JSlider) source;
+		double value = slider.getValue();
+		String slidername = slider.getName();
+		switch (slidername) {
+		case Command.SLIDER_CHANGE_SIZE:
+			model.setRadiusHead(value);
+			break;
+		case Command.SLIDER_CHANGE_EYES:
+			view.changeEyes(value);
+			break;
+		}
+//		System.out.println("Slider bewegt: " + slidername + " → Wert: " + value);
+		
+		view.prepareDrawing();
+		view.repaint();
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+	    switch (event.getActionCommand()) {
+	        case Command.SMILEY_SIZE_INC:
+	            view.changeSize(0.1);
+			    view.prepareDrawing();
+	            break;
+	        case Command.SMILEY_SIZE_DEC:
+	            view.changeSize(-0.1);
+	            view.prepareDrawing();
+	            break;
+	        case Command.SMILEY_EYE_INC:
+	            view.changeEyes(0.1);
+	            break;
+	        case Command.SMILEY_EYE_DEC:
+	            view.changeEyes(-0.1);
+	            break;
+	    }
+	    view.repaint();
+	}
+}
---
+++ pr2/OCHMANN.java.project/PR2AG/src/pr2/a09/SmileyModel.java
@@ -0,0 +1,67 @@
+package pr2.a09;
+
+import java.awt.geom.Point2D;
+
+public class SmileyModel {
+	protected Point2D position;
+	protected boolean smile;
+	protected double radiusHead;
+	protected double radiusEye;
+	protected double radiusPupil;
+	
+	public SmileyModel() {
+		position = new Point2D.Double(0, 0);
+		radiusHead = 100;
+		smile = true;
+		radiusEye = getRadiusHead() * 0.25;
+		radiusPupil = getRadiusEye() * 0.3;
+	}
+	
+	public SmileyModel(Point2D.Double position, double radiusHead, boolean smile) {
+		this.position = position;
+		this.radiusHead = 100;
+		this.smile = true;
+		radiusEye = getRadiusHead() * 0.25;
+		radiusPupil = getRadiusEye() * 0.5 / 2;
+	}
+	
+	public double getRadiusEye(){
+		return radiusEye;
+	}
+	
+	public void setRadiusEye(double newRadiusEye) {
+		radiusEye = newRadiusEye;
+	}
+	
+	public double getRadiusPupil(){
+		return radiusPupil;
+	}
+	
+	public void setRadiusPupil(double newRadiusPupil) {
+		radiusPupil = newRadiusPupil;
+	}
+	
+	public double getRadiusHead(){
+		return radiusHead;
+	}
+	
+	public void setRadiusHead(double newRadiusHead) {
+		radiusHead = newRadiusHead;
+	}
+	
+	public boolean getSmile(){
+		return smile;
+	}
+	
+	public void setSmile(boolean newSmile) {
+		smile = newSmile;
+	}
+	
+	public Point2D getPosition(){
+		return position;
+	}
+	
+	public void setPosition(Point2D newPosition) {
+		position = newPosition;
+	}	
+}
